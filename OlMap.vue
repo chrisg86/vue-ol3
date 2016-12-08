5b0a48500cf2f5e2b174a8bc8d4032ef9e389ec3
chris <chris.greindl@googlemail.com>
2016-12-08 19:31:28 +0100

    Added vue file

---
+++ OlMap.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="ol-map" :id="id"></div>
+</template>
+
+<script>
+let ol = require('openlayers')
+require('openlayers/dist/ol.css')
+
+export default {
+  name: 'ol-map',
+
+  props: {
+    lat: {
+      required: true,
+      type: Number
+    },
+
+    lon: {
+      required: true,
+      type: Number
+    },
+
+    zoom: {
+      required: false,
+      type: Number,
+      default: 4
+    }
+  },
+
+  data () {
+    return {
+      map: {
+        type: Object,
+        default: {}
+      }
+    }
+  },
+
+  computed: {
+    id () {
+      return this.$options.name + '-' + this._uid
+    }
+  },
+
+  mounted () {
+    this.map = new ol.Map({
+      target: this.id,
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.lon, this.lat]),
+        zoom: this.zoom
+      })
+    })
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
